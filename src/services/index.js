d1f47951b924c53351dd1ef2a6e769ad31ca8fdc
haamip <haamiphillips@gmail.com>
2026-07-02 22:38:09 +1200

    Add service barrel exports

---
+++ src/services/index.js
@@ -0,0 +1,14 @@
+export * from "./engines/aiCoachEngine";
+export * from "./engines/coachIntelligenceEngine";
+export * from "./engines/exerciseInsightEngine";
+export * from "./engines/progressionEngine";
+export * from "./engines/prEngineV2";
+export * from "./engines/workoutAnalyticsEngine";
+
+export * from "./generators/demoAthleteGenerator";
+export * from "./generators/demoSeedData";
+
+export * from "./repositories/trackfitDataLayer";
+
+export * from "./utils/storage";
+export * from "./workoutEngine";
